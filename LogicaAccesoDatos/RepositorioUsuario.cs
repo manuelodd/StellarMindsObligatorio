1cc72f6e370b5473a28898e4b8ef06aa9bd40163
oddmushroom <hackmanu2@gmail.com>
2026-04-17 17:04:31 -0300

    added some comments

--- LogicaAccesoDatos/RepositorioUsuario.cs
+++ LogicaAccesoDatos/RepositorioUsuario.cs
@@ -6,14 +6,16 @@ namespace LogicaAccesoDatos
     public class RepositorioUsuario
     {
 
-
+        // el "almacen" de _usuarios que teniamos en sistema ahora esta en su correspondiente repositorio"
         private List<Usuario> _usuarios;
         
+        // que se inicializa al instanciar RepositorioUsuario
         public RepositorioUsuario()
         {
             _usuarios = new List<Usuario>();
         }
 
+        // el alta se implementa acá
         public void AltaUsuario(Usuario unUsuario) 
         {
             try
--- LogicaAplicacion/CasosDeUso/UsuarioCU/AltaUsuarioCU.cs
+++ LogicaAplicacion/CasosDeUso/UsuarioCU/AltaUsuarioCU.cs
@@ -8,7 +8,7 @@ namespace LogicaAplicacion.CasosDeUso.UsuarioCU
 
         private RepositorioUsuario repositorio;
 
-
+        // al instanciar un AltaUsuarioCU se crea una List<Usuario> _usuarios 
         public AltaUsuarioCU()
         {
             repositorio = new RepositorioUsuario();
@@ -17,6 +17,7 @@ namespace LogicaAplicacion.CasosDeUso.UsuarioCU
 
         public void AltaUsuario(Usuario unUsuario)
         {
+            // y acá el alta usuario de repositorio (RepositorioUsuario) lo agrega a _usuarios
             repositorio.AltaUsuario(unUsuario);
         }
     }
