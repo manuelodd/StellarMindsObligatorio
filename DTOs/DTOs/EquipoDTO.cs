60f62893f801097f3be1f9e2d789f3fc423cf69d
manuelodd <hackmanu2@gmail.com>
2026-05-19 16:33:20 -0300

    dto generico de equipo

--- DTOs/DTOs/EquipoDTO.cs
+++ DTOs/DTOs/EquipoDTO.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace DTOs.DTOs
 {
-    internal class EquipoDTO
+    public class EquipoDTO
     {
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int CantDisp { get; set; }
+
+        //evaluar el tipo del objeto (tele, montu, cam, ocu)
+        public string Tipo { get; set; }
     }
 }
