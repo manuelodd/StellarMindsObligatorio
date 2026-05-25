0ae7100560577f9de1c473965a135f933b4618c1
manuelodd <hackmanu2@gmail.com>
2026-05-25 12:09:42 -0300

    PrestamoDTO y Mapper  ? ?

---
+++ DTOs/DTOs/PrestamoDTO.cs
@@ -0,0 +1,21 @@
+﻿using StellarMinds.Entities;
+using StellarMinds.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTOs.DTOs
+{
+    public class PrestamoDTO
+    {
+        public int Id { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public EstadoPrestamo Estado { get; set; }
+        public Telescopio Telescopio { get; set; } = null;
+        public Montura Montura { get; set; } = null;
+        public Camara Camara { get; set; } = null;
+        public Ocular Ocular { get; set; } = null;
+
+    }
+}
---
+++ DTOs/Mappers/PrestamoDTOMapper.cs
@@ -0,0 +1,49 @@
+﻿using DTOs.DTOs;
+using StellarMinds.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTOs.Mappers
+{
+
+    /*        public int Id { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public EstadoPrestamo Estado { get; set; }
+        public Telescopio Telescopio { get; set; } = null;
+        public Montura Montura { get; set; } = null;
+        public Camara Camara { get; set; } = null;
+        public Ocular Ocular { get; set; } = null;*/
+    public class PrestamoDTOMapper
+        {
+            public static PrestamoDTO ToDTO(Prestamo unPrestamo)
+            {
+                return new PrestamoDTO
+                {
+                    Id = unPrestamo.Id,
+                    FechaInicio = unPrestamo.FechaInicio,
+                    FechaFin = unPrestamo.FechaFin,
+                    Estado = unPrestamo.Estado,
+                    Telescopio = unPrestamo.Telescopio,
+                    Montura = unPrestamo.Montura,
+                    Camara = unPrestamo.Camara,
+                    Ocular = unPrestamo.Ocular
+                };
+            }
+        public static Prestamo FromDTO(PrestamoDTO dto)
+        {
+            return new Prestamo
+            {
+                Id = dto.Id,
+                FechaInicio = dto.FechaInicio,
+                FechaFin = dto.FechaFin,
+                Estado = dto.Estado,
+                Telescopio = dto.Telescopio,
+                Montura = dto.Montura,
+                Camara = dto.Camara,
+                Ocular = dto.Ocular
+            };
+        }
+    }
+    }
