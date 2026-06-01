a403f75b28525a92870fcc09f9ca6485aaa0d38f
manuelodd <hackmanu2@gmail.com>
2026-06-01 19:41:44 -0300

    Fix databse

---
+++ LogicaAccesoDatos/Migrations/20260601214137_AgregarSocioAPrestamo.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LogicaAccesoDatos.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarSocioAPrestamo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SocioId",
+                table: "Prestamos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prestamos_SocioId",
+                table: "Prestamos",
+                column: "SocioId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Prestamos_Usuarios_SocioId",
+                table: "Prestamos",
+                column: "SocioId",
+                principalTable: "Usuarios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Prestamos_Usuarios_SocioId",
+                table: "Prestamos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Prestamos_SocioId",
+                table: "Prestamos");
+
+            migrationBuilder.DropColumn(
+                name: "SocioId",
+                table: "Prestamos");
+        }
+    }
+}
