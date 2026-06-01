cd4afa6a1c2d670e6dc950ef095d83ddd05baf24
manuelodd <hackmanu2@gmail.com>
2026-06-01 17:40:48 -0300

    Viewmodel para devolucion

---
+++ DTOs/AuxiliarViewmodel/PrestamoDevolucionViewmodel.cs
@@ -0,0 +1,17 @@
+﻿using DTOs.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTOs.AuxiliarViewmodel
+{
+    
+    public class PrestamoDevolucionViewmodel
+    {
+            public int? SocioId { get; set; }
+            public int? PrestamoId { get; set; }
+            public IEnumerable<UsuarioDTO> Socios { get; set; }
+            public IEnumerable<PrestamoDTO> Prestamos { get; set; }
+        }
+    
+}
