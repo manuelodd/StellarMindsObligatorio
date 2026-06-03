2597903fed5691b1b2a7116d2419e1a9abbe4a8d
manuelodd <hackmanu2@gmail.com>
2026-06-03 17:03:45 -0300

    Mapper objeto celeste

---
+++ DTOs/Mappers/ObjetoCelesteDTOMapper.cs
@@ -0,0 +1,33 @@
+﻿using Dominio.Entities;
+using DTOs.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTOs.Mappers
+{
+    public class ObjetoCelesteDTOMapper
+    {
+        public static ObjetoCelesteDTO ToDTO(ObjetoCeleste objeto)
+        {
+            return new ObjetoCelesteDTO
+            {
+                Id = objeto.Id,
+                Nombre = objeto.Nombre,
+                Tipo = objeto.Tipo,
+                MagnitudAparente = objeto.MagnitudAparente
+            };
+        }
+
+        public static ObjetoCeleste FromDTO(ObjetoCelesteDTO dto)
+        {
+            return new ObjetoCeleste
+            {
+                Id = dto.Id,
+                Nombre = dto.Nombre,
+                Tipo = dto.Tipo,
+                MagnitudAparente = dto.MagnitudAparente
+            };
+        }
+    }
+}
