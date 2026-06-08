96020b52dc5eeaad68413455f90bc779b71f7096
manuelodd <hackmanu2@gmail.com>
2026-06-08 19:50:03 -0300

    Todas altas, edits equipos, y controllerusuario mvc>api

--- StellarMindsWebApi/Controllers/EquipoController.cs
+++ StellarMindsWebApi/Controllers/EquipoController.cs
@@ -1,8 +1,10 @@
-﻿using Dominio.InterfacesRepositorio;
+﻿using Dominio.Exceptions;
+using Dominio.InterfacesRepositorio;
 using DTOs.DTOs;
 using LogicaAplicacion.InterfacesCasosDeUso;
 using Microsoft.AspNetCore.Mvc;
 using StellarMinds.Entities;
+using System.Data.Common;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -53,10 +55,222 @@ namespace StellarMindsWebApi.Controllers
 
         // GET: api/<EquipoController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult GetAll()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                return Ok(findAllCU.Execute());
+            }
+            catch (DbException)
+            {
+                return StatusCode(500, "Error de base de datos.");
+            }
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                return Ok(buscarEquipoIDCU.Execute(id));
+            }
+            catch (EntityNotFoundException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (DbException)
+            {
+                return StatusCode(500, "Error de base de datos.");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                deleteEquipoCU.Execute(id);
+                return NoContent();
+            }
+            catch (EntityNotFoundException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (InvalidEquipoException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (DbException)
+            {
+                return StatusCode(500, "Error de base de datos.");
+            }
+        }
+
+        // ALTAS
+
+        [HttpPost("telescopios")]
+        public ActionResult<TelescopioDTO> CreateTelescopio(TelescopioDTO dto)
+        {
+            try
+            {
+                altaCU.Execute(dto);
+                return Ok();
+            }
+            catch (InvalidEquipoException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (DbException)
+            {
+                return StatusCode(500, "Error de base de datos.");
+            }
+        }
+
+        [HttpPost("monturas")]
+        public ActionResult<MonturaDTO> CreateMontura(MonturaDTO dto)
+        {
+            try
+            {
+                altaCU.Execute(dto);
+                return Ok();
+            }
+            catch (InvalidEquipoException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (DbException)
+            {
+                return StatusCode(500, "Error de base de datos.");
+            }
+        }
+
+        [HttpPost("camaras")]
+        public ActionResult<CamaraDTO> CreateCamara(CamaraDTO dto)
+        {
+            try
+            {
+                altaCU.Execute(dto);
+                return Ok();
+            }
+            catch (InvalidEquipoException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (DbException)
+            {
+                return StatusCode(500, "Error de base de datos.");
+            }
+        }
+
+        [HttpPost("oculares")]
+        public ActionResult<OcularDTO> CreateOcular(OcularDTO dto)
+        {
+            try
+            {
+                altaCU.Execute(dto);
+                return Ok();
+            }
+            catch (InvalidEquipoException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (DbException)
+            {
+                return StatusCode(500, "Error de base de datos.");
+            }
+        }
+
+        // EDITS
+
+        [HttpPut("telescopios")]
+        public ActionResult<TelescopioDTO> EditTelescopio(TelescopioDTO dto)
+        {
+            try
+            {
+                editarTelescopioCU.Execute(dto);
+                return Ok();
+            }
+            catch (EntityNotFoundException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (InvalidEquipoException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (DbException)
+            {
+                return StatusCode(500, "Error de base de datos.");
+            }
+        }
+
+        [HttpPut("monturas")]
+        public ActionResult<MonturaDTO> EditMontura(MonturaDTO dto)
+        {
+            try
+            {
+                editarMonturaCU.Execute(dto);
+                return Ok();
+            }
+            catch (EntityNotFoundException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (InvalidEquipoException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (DbException)
+            {
+                return StatusCode(500, "Error de base de datos.");
+            }
+        }
+
+        [HttpPut("camaras")]
+        public ActionResult<CamaraDTO> EditCamara(CamaraDTO dto)
+        {
+            try
+            {
+                editarCamaraCU.Execute(dto);
+                return Ok();
+            }
+            catch (EntityNotFoundException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (InvalidEquipoException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (DbException)
+            {
+                return StatusCode(500, "Error de base de datos.");
+            }
+        }
+
+        [HttpPut("oculares")]
+        public ActionResult<OcularDTO> EditOcular(OcularDTO dto)
+        {
+            try
+            {
+                editarOcularCU.Execute(dto);
+                return Ok();
+            }
+            catch (EntityNotFoundException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (InvalidEquipoException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (DbException)
+            {
+                return StatusCode(500, "Error de base de datos.");
+            }
+        }
+
+
 
         /*
         // GET api/<EquipoController>/5
--- StellarMindsWebApi/Controllers/UsuarioController.cs
+++ StellarMindsWebApi/Controllers/UsuarioController.cs
@@ -2,6 +2,8 @@
 using DTOs.DTOs;
 using LogicaAplicacion.InterfacesCasosDeUso;
 using Microsoft.AspNetCore.Mvc;
+using StellarMinds.Entities;
+using System.Data.Common;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,8 +17,8 @@ namespace StellarMindsWebApi.Controllers
         private IAltaUsuario altaCU;
         private IListarUsuarios findAllCU;
 
-        public UsuarioController    (ILoginUsuario loginu, 
-                                    IAltaUsuario altau, 
+        public UsuarioController(ILoginUsuario loginu,
+                                    IAltaUsuario altau,
                                     IListarUsuarios findAllCu)
         {
             this.loginU = loginu;
@@ -50,21 +52,67 @@ namespace StellarMindsWebApi.Controllers
                 return Ok(listado);
             }
             //primer catch, si explota la db (por x motivo), mala cadena de conexion
-            catch (System.Data.Common.DbException)
+            catch (DbException)
             {
                 return StatusCode(500, "Internal server error.");
             }
-                //segundo catch, si la logica de negocio lanza excepcion (badrequest = 400)
-                catch(InvalidUserException ex)
+            //segundo catch, si la logica de negocio lanza excepcion (badrequest = 400)
+            catch (InvalidUserException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            //un "safety net" en caso de que se lance una exception generica de algun lado
+            catch (Exception ex)
+            {
                 {
                     return BadRequest(ex.Message);
                 }
+            }
         }
 
+            [HttpPost("login")]
+            public ActionResult<UsuarioDTO> Login(string username, string pass)
+            {
+                try
+                {
+                    Usuario usuario = loginU.Execute(username, pass);
 
+                    return Ok(usuario);
+                }
+                catch (InvalidUserException ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+                catch (DbException)
+                {
+                    return StatusCode(500, "Error de base de datos.");
+         
+                }
+                catch(Exception ex) 
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
 
+            [HttpPost]
+            public ActionResult<UsuarioDTO> Post(UsuarioDTO dto)
+            {
+                try
+                {
+                    altaCU.Execute(dto);
+                    return Ok();
+                }
+                catch (InvalidUserException ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+                catch (DbException)
+                {
+                    return StatusCode(500, "Error de base de datos.");
+                }
+            }
 
-
+            /*
         // GET api/<UsuarioController>/5
         [HttpGet("{id}")]
         public string Get(int id)
@@ -89,5 +137,6 @@ namespace StellarMindsWebApi.Controllers
         public void Delete(int id)
         {
         }
+            */
+        }
     }
-}
