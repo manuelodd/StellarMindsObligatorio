8d00207ae1be318f6690debc4b05732d09fdd479
manuelodd <hackmanu2@gmail.com>
2026-06-09 19:54:03 -0300

    ObjetoCelesteController

--- StellarMindsWebAPP/Controllers/ObservacionController.cs
+++ StellarMindsWebAPP/Controllers/ObservacionController.cs
@@ -1,18 +1,52 @@
 ﻿
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using StellarMindsWebApp.AuxiliarViewmodel;
+using StellarMindsWebAPP.Auxiliar;
+using StellarMindsWebAPP.Enums;
+using StellarMindsWebAPP.Models;
 
 namespace StellarMindsWebAPP.Controllers
 {
     public class ObservacionController : BaseController
     {
 
+        private string baseUrl = "http://localhost:5077/api/Observacion";
 
-
-        // GET: ObservacionController
-        public ActionResult Index()
+        public IActionResult RankingObjetosCelestes()
         {
-            return View();
+            HttpResponseMessage respuesta = HttpClientAuxiliar.EnviarSolicitud($"{baseUrl}/ranking", HttpVerbos.GET);
+
+            if (respuesta.IsSuccessStatusCode)
+            {
+                string json = HttpClientAuxiliar.ObtenerBody(respuesta);
+                List<RankObjetosCelestesModel> listado = JsonConvert.DeserializeObject<List<RankObjetosCelestesModel>>(json);
+                return View(listado);
+            }
+            ViewBag.Error = HttpClientAuxiliar.ObtenerBody(respuesta);
+            return View(new List<RankObjetosCelestesModel>());
+        }
+
+        public IActionResult Create()
+        {
+            ObservacionAltaViewmodel vm = new ObservacionAltaViewmodel();
+
+            int socioId = idLogeado();
+
+            HttpResponseMessage prestamos = HttpClientAuxiliar.EnviarSolicitud($"http://localhost:5077/api/Prestamo/socio/{socioId}", HttpVerbos.GET);
+            if (prestamos.IsSuccessStatusCode)
+            {
+                vm.Prestamos = JsonConvert.DeserializeObject<List<PrestamoModel>>(HttpClientAuxiliar.ObtenerBody(prestamos));
+            }
+
+            HttpResponseMessage objetos = HttpClientAuxiliar.EnviarSolicitud("http://localhost:5077/api/ObjetoCeleste", HttpVerbos.GET);
+            if (objetos.IsSuccessStatusCode)
+            {
+                vm.ObjetosCelestes = JsonConvert.DeserializeObject<List<ObjetoCelesteModel>>(HttpClientAuxiliar.ObtenerBody(objetos));
+            }
+
+            return View(vm);
         }
 
         // GET: ObservacionController/Details/5
---
+++ StellarMindsWebApi/Controllers/ObjetoCelesteController.cs
@@ -0,0 +1,32 @@
+﻿using LogicaAplicacion.InterfacesCasosDeUso;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.Common;
+
+namespace StellarMindsWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ObjetoCelesteController : ControllerBase
+    {
+        private IListarObjetosCelestes listarObjetosCelestesCU;
+
+        public ObjetoCelesteController(
+            IListarObjetosCelestes listarObjetosCelestesCu)
+        {
+            listarObjetosCelestesCU = listarObjetosCelestesCu;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                return Ok(listarObjetosCelestesCU.Execute());
+            }
+            catch (DbException)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
