a51af6af4b891713b12231d1e20eee5d695c65e9
manuelodd <hackmanu2@gmail.com>
2026-06-08 21:07:28 -0300

    Todos los CU en el API

--- StellarMindsWebApi/Program.cs
+++ StellarMindsWebApi/Program.cs
@@ -2,7 +2,11 @@
 using Dominio.InterfacesRepositorio;
 using LogicaAccesoDatos.EntityFramework;
 using LogicaAccesoDatos.EntityFramework.Repositorios;
+using LogicaAplicacion.CasosDeUso.CUAuditoriaPrestamo;
 using LogicaAplicacion.CasosDeUso.CUEquipo;
+using LogicaAplicacion.CasosDeUso.CUObjetoCeleste;
+using LogicaAplicacion.CasosDeUso.CUObservacion;
+using LogicaAplicacion.CasosDeUso.CUPrestamo;
 using LogicaAplicacion.CasosDeUso.CUUsuario;
 using LogicaAplicacion.InterfacesCasosDeUso;
 using Microsoft.EntityFrameworkCore;
@@ -44,19 +48,50 @@ namespace StellarMindsWebApi
             //ini repositorios
             builder.Services.AddScoped<IRepositorioUsuario, RepositorioUsuarioEF>();
             builder.Services.AddScoped<IRepositorioEquipo, RepositorioEquipoEF>();
+            builder.Services.AddScoped<IRepositorioPrestamo, RepositorioPrestamoEF>();
+            builder.Services.AddScoped<IRepositorioAuditoriaPrestamo, RepositorioAuditoriaPrestamoEF>();
+            builder.Services.AddScoped<IRepositorioObservacion, RepositorioObservacionEF>();
+            builder.Services.AddScoped<IRepositorioObjetoCeleste, RepositorioObjetoCelesteEF>();
             //ini casos de uso USUARIOS
             builder.Services.AddScoped<IAltaUsuario, AltaUsuarioCU>();
             builder.Services.AddScoped<IListarUsuarios, ListarUsuariosCU>();
             builder.Services.AddScoped<ILoginUsuario, LoginUsuarioCU>();
+            builder.Services.AddScoped<IFindUsuById, FindUsuByIdCU>();
+            builder.Services.AddScoped<IListarSociosByTelescopio, ListarSociosByTelescopioCU>();
+            builder.Services.AddScoped<IListarCoordinadores, ListarCoordinadoresCU>();
+
             //ini casos de uso EQUIPOS
             builder.Services.AddScoped<IAltaEquipo, AltaEquipoCU>();
             builder.Services.AddScoped<IListarEquipos, ListarEquiposCU>();
+            builder.Services.AddScoped<IListarTelescopios, ListarTelescopiosCU>(); //lista telescopios cuya cant disp > 1
+            builder.Services.AddScoped<IListarTelescopiosToList, ListarTelescopiosToListCU>(); // devuelve todos
+            builder.Services.AddScoped<IListarMonturas, ListarMonturasCU>();
+            builder.Services.AddScoped<IListarCamaras, ListarCamarasCU>();
+            builder.Services.AddScoped<IListarOculares, ListarOcularesCU>();
             builder.Services.AddScoped<IBuscarEquipoPorID, BuscarEquipoPorIDCU>();
             builder.Services.AddScoped<IEditarTelescopio, EditarTelescopioCU>();
             builder.Services.AddScoped<IEditarMontura, EditarMonturaCU>();
             builder.Services.AddScoped<IEditarCamara, EditarCamaraCU>();
             builder.Services.AddScoped<IEditarOcular, EditarOcularCU>();
             builder.Services.AddScoped<IDeleteEquipo, DeleteEquipoCU>();
+            //ini casos de uso PRESTAMOS
+            builder.Services.AddScoped<IAltaPrestamo, AltaPrestamoCU>();
+            builder.Services.AddScoped<IListarPrestamos, ListarPrestamosCU>();
+            builder.Services.AddScoped<IReturnPrestamo, ReturnPrestamoCU>();
+            builder.Services.AddScoped<IListarPrestamosSocio, ListarPrestamosSocioCU>();
+            builder.Services.AddScoped<IListarPrestamosSocioEntreFechas, ListarPrestamosSocioEntreFechasCU>();
+            builder.Services.AddScoped<IFindPrestamoById, FindPrestamoById>();
+
+            //ini casos de uso AUDITORIAS PRESTAMO
+            builder.Services.AddScoped<IListarAuditoriasPrestamo, ListarAuditoriasPrestamoCU>();
+            builder.Services.AddScoped<IListarAuditoriasByCoordinador, ListarAuditoriasByCoordinadorCU>();
+            builder.Services.AddScoped<IFindAuditoriaById, FindAuditoriaById>();
+
+            //ini casos de uso OBJETOS CELESTES
+            builder.Services.AddScoped<IListarObjetosCelestes, ListarObjetosCelestesCU>();
+            builder.Services.AddScoped<IRankObjetosCelestes, RankObjetosCelestesCU>();
+            //ini casos de uso OBSERVACIONES
+            builder.Services.AddScoped<IAltaObservacion, AltaObservacionCU>();
 
             var app = builder.Build();
 
